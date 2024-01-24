3bdef163af76a236c9ab00dd7cd2044ffbcdd2fb
Aurelie Gedeon <agedeon540@gmail.com>
2024-01-23 21:56:37 -0600

    AG-2 set up new components folder; set up navbar component

---
+++ code-ff/project2/src/components/navbar/navbar.jsx
@@ -0,0 +1,12 @@
+import "./navbar.css"
+
+export default function Navbar(){
+
+    return(
+        <>
+          <div className="navbar">
+            <p className="team-name">Team CloudBloom</p>
+          </div>  
+        </>
+    )
+}
