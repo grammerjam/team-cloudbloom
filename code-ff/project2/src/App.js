0e084164b8345e1a3a4386e9fac9654d51c7cb89
Aurelie Gedeon <agedeon540@gmail.com>
2024-01-23 23:32:02 -0600

    AG-8 added background image

--- code-ff/project2/src/App.js
+++ code-ff/project2/src/App.js
@@ -1,8 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Home from "./scene/home/home";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Home />
+    </>
+  );
 }
 
 export default App;
