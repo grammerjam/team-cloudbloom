e98ec9a94c34f404905e612f8975824f7afd0721
Aurelie Gedeon <agedeon540@gmail.com>
2024-02-02 22:23:36 -0600

    AG-7 fixed alignment of form on page

--- code-ff/project2/src/components/Form/Form.jsx
+++ code-ff/project2/src/components/Form/Form.jsx
@@ -4,11 +4,12 @@ export default function Form(name, card_number, exp_date, cvc) {
 
     return(
         <>
-            <p>hello</p>
-            <div className="form">
-                <form action="">Heelo</form>
-                <form action="">Hello</form>
-                <form action="">Heloo</form>
+            <div className="form-container">
+                <div className="form">
+                    <form action="">Heelo</form>
+                    <form action="">Hello</form>
+                    <form action="">Heloo</form>
+                </div>
             </div>
         </>
     )
