132de9503eb23e73ef7a6618ec827391c917a3fd
Aurelie Gedeon <agedeon540@gmail.com>
2024-02-02 22:37:41 -0600

    AG-7 added form with labels and inputs

--- code-ff/project2/src/components/Form/Form.jsx
+++ code-ff/project2/src/components/Form/Form.jsx
@@ -6,9 +6,24 @@ export default function Form(name, card_number, exp_date, cvc) {
         <>
             <div className="form-container">
                 <div className="form">
-                    <form action="">Heelo</form>
-                    <form action="">Hello</form>
-                    <form action="">Heloo</form>
+                    <form action="">
+                        <div className="name">
+                            <label for="name">CARDHOLDER NAME</label>
+                            <input type="text" required />
+                        </div>
+                        <div className="number">
+                            <label for="number">CARDHOLDER NUMBER</label>
+                            <input type="text" required />
+                        </div>
+                        <div className="exp-date">
+                            <label for="exp-date">EXP. DATE (MM/YY)</label>
+                            <input type="text" required />
+                        </div>
+                        <div className="cvc">
+                            <label for="cvc">CVC</label>
+                            <input type="text" required />
+                        </div>
+                    </form>
                 </div>
             </div>
         </>
