528d3b4c21e900f85cdaf0e3d371048d0961c6f4
Aurelie Gedeon <agedeon540@gmail.com>
2024-02-02 23:21:39 -0600

    AG-7 input styling

--- code-ff/project2/src/components/Form/Form.jsx
+++ code-ff/project2/src/components/Form/Form.jsx
@@ -7,21 +7,22 @@ export default function Form(name, card_number, exp_date, cvc) {
             <div className="form-container">
                 <div className="form">
                     <form action="">
-                        <div className="name">
+                        <div className="form-input">
                             <label for="name">CARDHOLDER NAME</label>
-                            <input type="text" required />
+                            <input type="text" name="name" id="" placeholder="e.g. Jane Appleseed" required />
                         </div>
-                        <div className="number">
+                        <div className="form-input">
                             <label for="number">CARDHOLDER NUMBER</label>
-                            <input type="text" required />
+                            <input type="text" name="number" id="" placeholder="e.g. 1234 4678 9123 0000" required />
                         </div>
-                        <div className="exp-date">
+                        <div className="form-input">
                             <label for="exp-date">EXP. DATE (MM/YY)</label>
-                            <input type="text" required />
+                            <input type="text" name="exp-date-month" id="exp-date-month" placeholder="MM" required />
+                            <input type="text" name="exp-date-year" id="exp-date-year" placeholder="YY" required />
                         </div>
-                        <div className="cvc">
+                        <div className="form-input">
                             <label for="cvc">CVC</label>
-                            <input type="text" required />
+                            <input type="text" name="" id="" placeholder="e.g. 123" required />
                         </div>
                     </form>
                 </div>
