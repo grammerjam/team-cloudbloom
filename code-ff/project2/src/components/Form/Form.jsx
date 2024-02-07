75e376dade880918726eed9984851f0b24afa021
Aurelie Gedeon <agedeon540@gmail.com>
2024-02-06 22:49:58 -0600

    AG-7 made function more dynamic to apply to all inputs

--- code-ff/project2/src/components/Form/Form.jsx
+++ code-ff/project2/src/components/Form/Form.jsx
@@ -1,38 +1,55 @@
 import "./Form.css"
 import { useState } from "react"
 
-export default function Form(name, card_number, exp_date, cvc) {
+export default function Form() {
 
-    const [formData, setFormData] = useState("")
+    const [formData, setFormData] = useState({
+        name: '',
+        cardNumber: '',
+        expDateMonth: '',
+        expDateYear: '',
+        cvc: ''
+    })
 
     const handleInputChange = (e) => {
-        setFormData(e.target.value)
+        const { name,  value } = e.target
+        setFormData({
+            ...formData, 
+            [name] : value
+        })
     }
 
     return(
         <>
             <div className="form-container">
+            <div>
+                <p>{formData.name}</p>
+                <p>{formData.cardNumber}</p>
+                <p>{formData.expDateMonth}</p>
+                <p>{formData.expDateYear}</p>
+                <p>{formData.cvc}</p>
+            </div>
                 <div className="form">
                     <form action="">
                         <div className="form-input">
                             <label for="name">CARDHOLDER NAME</label>
-                            <input type="text" name="name" id="name" value={formData} onChange={handleInputChange} placeholder="e.g. Jane Appleseed" required />
+                            <input type="text" name="name" id="name" value={formData.name} onChange={handleInputChange} placeholder="e.g. Jane Appleseed" required />
                         </div>
                         <div className="form-input">
                             <label for="number">CARDHOLDER NUMBER</label>
-                            <input type="text" name="number" id="number" maxLength={12} placeholder="e.g. 1234 4678 9123 0000" required />
+                            <input type="text" name="cardNumber" id="number" value={formData.cardNumber} onChange={handleInputChange} maxLength={12} placeholder="e.g. 1234 4678 9123 0000" required />
                         </div>
                         <div className="date-cvc">
                             <div className="exp-date form-input">
                                 <label for="exp-date">EXP. DATE (MM/YY)</label>
                                 <div className="date">
-                                    <input type="text" name="exp-date-month" id="exp-date-month" placeholder="MM" maxLength={2} required />
-                                    <input type="text" name="exp-date-year" id="exp-date-year" placeholder="YY" maxLength={2} required />
+                                    <input type="text" name="expDateMonth" id="exp-date-month" value={formData.expDateMonth} onChange={handleInputChange} placeholder="MM" maxLength={2} required />
+                                    <input type="text" name="expDateYear" id="exp-date-year" value={formData.expDateYear} onChange={handleInputChange} placeholder="YY" maxLength={2} required />
                                 </div>
                             </div>
                             <div className="form-input cvc">
                                 <label for="cvc">CVC</label>
-                                <input type="text" name="cvc" id="cvc" placeholder="e.g. 123" maxLength={3} required />
+                                <input type="text" name="cvc" id="cvc" value={formData.cvc} onChange={handleInputChange} placeholder="e.g. 123" maxLength={3} required />
                             </div>
                         </div>
                         <button> Confirm </button>
